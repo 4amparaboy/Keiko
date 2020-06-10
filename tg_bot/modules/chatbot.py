939935f8a4841575d3d58d13078beb449f53e5c5
JyothisJayanth <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-10 15:49:49 +0530

    Update chatbot.py

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -90,8 +90,8 @@ def chatbot(bot: Bot, update: Update):
 __mod_name__ = "Chatbot"
 
 __help__ = """
-Chatbot utilizes the CoffeeHouse API and allows Keiko to talk back making your chat more interactive.
-This is an ongoing upgrade and is only available in your chats if you reach out to @YorktownEagleUnion and ask for it. 
+Chatbot utilizes the CoffeeHouse API and allows Kēiko to talk back making your chat more interactive.
+This is an ongoing upgrade and is only available in your chats if you reach out to the [Support Group](https://t.me/KeikoSupport) and ask for it. 
 In future we might make it open for any chat and controllable by group admins.
 Powered by CoffeeHouse (https://coffeehouse.intellivoid.net/) from @Intellivoid
 Commands: These only work for Keiko Staff users. 
