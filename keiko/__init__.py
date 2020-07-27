02c766c2beeaa058cb1245342ff34681bc1b15ff
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-27 19:50:10 +0530

    Added Mongo DB
    This is a Test
    
    commit

--- keiko/__init__.py
+++ keiko/__init__.py
@@ -62,7 +62,7 @@ if ENV:
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
-    DB_URI = os.environ.get('DATABASE_URL')
+    DB_URI = os.environ.get('DB_URL')
     DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
