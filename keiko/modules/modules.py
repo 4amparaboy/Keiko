5818fe715df2831fc51ef4e67aa2e9d4dae901ca
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-08-19 08:04:08 +0530

    Update modules.py

--- keiko/modules/modules.py
+++ keiko/modules/modules.py
@@ -88,11 +88,9 @@ def listmodules(bot: Bot, update: Update):
     message.reply_text(module_list, parse_mode=ParseMode.HTML)
 
 
-LOAD_HANDLER = CommandHandler("load", load)
 UNLOAD_HANDLER = CommandHandler("unload", unload)
 LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules)
 
-dispatcher.add_handler(LOAD_HANDLER)
 dispatcher.add_handler(UNLOAD_HANDLER)
 dispatcher.add_handler(LISTMODULES_HANDLER)
 
